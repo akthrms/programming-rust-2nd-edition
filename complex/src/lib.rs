64b344303fcda6cc2d81bef9a38bec89ecad1d50
akthrms <akt.hrms@gmail.com>
2022-03-06 11:04:24 +0900

    update

---
+++ complex/src/lib.rs
@@ -0,0 +1,65 @@
+use std::ops::{Add, AddAssign, Neg};
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+struct Complex<T> {
+    re: T,
+    im: T,
+}
+
+// impl<T> Add for Complex<T>
+// where
+//     T: Add<Output = T>,
+// {
+//     type Output = Complex<T>;
+//
+//     fn add(self, rhs: Self) -> Self::Output {
+//         Complex {
+//             re: self.re + rhs.re,
+//             im: self.im + rhs.im,
+//         }
+//     }
+// }
+
+impl<L, R> Add<Complex<R>> for Complex<L>
+where
+    L: Add<R>,
+{
+    type Output = Complex<L::Output>;
+
+    fn add(self, rhs: Complex<R>) -> Self::Output {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im,
+        }
+    }
+}
+
+impl<T> Neg for Complex<T>
+where
+    T: Neg<Output = T>,
+{
+    type Output = Complex<T>;
+
+    fn neg(self) -> Self::Output {
+        Complex {
+            re: -self.re,
+            im: -self.im,
+        }
+    }
+}
+
+impl<T> AddAssign for Complex<T>
+where
+    T: AddAssign<T>,
+{
+    fn add_assign(&mut self, rhs: Self) {
+        self.re += rhs.re;
+        self.im += rhs.im;
+    }
+}
+
+// impl<T: PartialEq> PartialEq for Complex<T> {
+//     fn eq(&self, other: &Self) -> bool {
+//         self.re == other.re && self.im == other.im
+//     }
+// }
