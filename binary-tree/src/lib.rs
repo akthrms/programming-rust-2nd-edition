2d0fedee141b9fc2f19b8b3411a8a82767607a2b
akthrms <akt.hrms@gmail.com>
2022-03-11 22:38:30 +0900

    update

--- binary-tree/src/lib.rs
+++ binary-tree/src/lib.rs
@@ -29,3 +29,45 @@ impl<T: Ord> BinaryTree<T> {
         }
     }
 }
+
+struct TreeIter<'a, T: 'a> {
+    unvisited: Vec<&'a TreeNode<T>>,
+}
+
+impl<'a, T: 'a> TreeIter<'a, T> {
+    fn push_left_edge(&mut self, mut tree: &'a BinaryTree<T>) {
+        while let BinaryTree::NotEmpty(ref node) = *tree {
+            self.unvisited.push(node);
+            tree = &node.left;
+        }
+    }
+}
+
+impl<T> BinaryTree<T> {
+    fn iter(&self) -> TreeIter<T> {
+        let mut iter = TreeIter {
+            unvisited: Vec::new(),
+        };
+        iter.push_left_edge(self);
+        iter
+    }
+}
+
+impl<'a, T: 'a> IntoIterator for &'a BinaryTree<T> {
+    type Item = &'a T;
+    type IntoIter = TreeIter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, T> Iterator for TreeIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let node = self.unvisited.pop()?;
+        self.push_left_edge(&node.right);
+        Some(&node.element)
+    }
+}
