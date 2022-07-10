cdac63f3a2eb1c425eff3f350a65e8093ac68ff9
akthrms <akt.hrms@gmail.com>
2022-07-10 20:17:52 +0900

    update

--- queue/src/lib.rs
+++ queue/src/lib.rs
@@ -1,8 +1,38 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>,
+}
+
+impl Queue {
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            std::mem::swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
+
+    pub fn new() -> Self {
+        Queue {
+            older: Vec::new(),
+            younger: Vec::new(),
+        }
     }
 }
