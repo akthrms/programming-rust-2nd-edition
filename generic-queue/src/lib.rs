5eb308c1ef6b275d2e064d4aaefc63cb2d52add9
akthrms <akt.hrms@gmail.com>
2022-07-10 20:34:24 +0900

    update

--- generic-queue/src/lib.rs
+++ generic-queue/src/lib.rs
@@ -1,8 +1,38 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub struct Queue<T> {
+    older: Vec<T>,
+    younger: Vec<T>,
+}
+
+impl<T> Queue<T> {
+    pub fn new() -> Self {
+        Queue {
+            older: Vec::new(),
+            younger: Vec::new(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn push(&mut self, t: T) {
+        self.younger.push(t);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            std::mem::swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        self.older.pop()
+    }
+
+    pub fn split(self) -> (Vec<T>, Vec<T>) {
+        (self.older, self.younger)
     }
 }
