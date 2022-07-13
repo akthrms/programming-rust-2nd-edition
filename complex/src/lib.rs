834277f42308a1f571ccf7605578571ade7b3b98
akthrms <akt.hrms@gmail.com>
2022-07-13 10:50:48 +0900

    update

--- complex/src/lib.rs
+++ complex/src/lib.rs
@@ -1,8 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::ops::Add;
+
+#[derive(Clone, Copy, Debug)]
+struct Complex<T> {
+    re: T,
+    im: T,
+}
+
+impl<L, R> Add<Complex<R>> for Complex<L>
+where
+    L: Add<R>,
+{
+    type Output = Complex<L::Output>;
+
+    fn add(self, rhs: Complex<R>) -> Self::Output {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im,
+        }
     }
 }
