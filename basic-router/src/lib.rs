db70d3358f065a869228eb4c79fc0013f0f45771
akthrms <akt.hrms@gmail.com>
2022-07-14 13:39:23 +0900

    update

--- basic-router/src/lib.rs
+++ basic-router/src/lib.rs
@@ -1,8 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::collections::HashMap;
+
+struct Request {
+    method: String,
+    url: String,
+    headers: HashMap<String, String>,
+    body: Vec<u8>,
+}
+
+struct Response {
+    code: u32,
+    headers: HashMap<String, String>,
+    body: Vec<u8>,
+}
+
+type Callback = Box<dyn Fn(&Request) -> Response>;
+
+struct BasicRouter {
+    routes: HashMap<String, Callback>,
+}
+
+impl BasicRouter {
+    fn new() -> Self {
+        BasicRouter {
+            routes: HashMap::new(),
+        }
+    }
+
+    fn add_route<C>(&mut self, url: &str, callback: C)
+    where
+        C: Fn(&Request) -> Response + 'static,
+    {
+        self.routes.insert(url.to_string(), Box::new(callback));
+    }
+
+    fn handle_request(&self, request: &Request) -> Response {
+        match self.routes.get(&request.url) {
+            None => unimplemented!(),
+            Some(callback) => callback(request),
+        }
     }
 }
