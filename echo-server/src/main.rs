84e0d120bb2c56dbdf28d9cbab61fba7f8bd62d7
akthrms <akt.hrms@gmail.com>
2022-07-20 09:54:30 +0900

    update

--- echo-server/src/main.rs
+++ echo-server/src/main.rs
@@ -1,3 +1,25 @@
+use std::{io, net::TcpListener, thread::spawn};
+
 fn main() {
-    println!("Hello, world!");
+    echo_main("127.0.0.1:17007").expect("error");
+}
+
+fn echo_main(addr: &str) -> io::Result<()> {
+    let listener = TcpListener::bind(addr)?;
+
+    println!("listening on {}", addr);
+
+    loop {
+        let (mut stream, addr) = listener.accept()?;
+
+        println!("connection received from {}", addr);
+
+        let mut write_stream = stream.try_clone()?;
+
+        spawn(move || {
+            io::copy(&mut stream, &mut write_stream).expect("error in client thread");
+
+            println!("connection closed");
+        });
+    }
 }
